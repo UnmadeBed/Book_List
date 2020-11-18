79b6c100d7e52ebc39451ed025346b29cb8121a7
unmadebed <kevin.archer@live.com>
2020-11-18 22:15:12 +0000

    add validation and error alert

--- app.js
+++ app.js
@@ -24,6 +24,27 @@ UI.prototype.addBookToList = function(book) {
 	list.appendChild(row);
 };
 
+// Show alert
+UI.prototype.showAlert = function(message, className) {
+	// Create div
+	const div = document.createElement('div');
+	// Add classes
+	div.className = `alert ${className}`;
+	// Add text
+	div.appendChild(document.createTextNode(message));
+	// Get parent
+	const container = document.querySelector('.container');
+	// Get form
+	const form = document.querySelector('#book-form');
+	// Insert alert
+	container.insertBefore(div, form);
+
+	// time out after 3 secs
+	setTimeout(function() {
+		document.querySelector('.alert').remove();
+	}, 3000);
+};
+
 // Clear fields
 UI.prototype.clearFields = function() {
 	document.getElementById('title').value = '';
@@ -44,11 +65,17 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
 	// Instantiate UI object
 	const ui = new UI();
 
-	// Add book to list
-	ui.addBookToList(book);
+	// Validate
+	if (title === '' || author === '' || isbn === '') {
+		// Error alert
+		ui.showAlert('Please fill in all fields', 'error');
+	} else {
+		// Add book to list
+		ui.addBookToList(book);
 
-	// Clear fields
-	ui.clearFields();
+		// Clear fields
+		ui.clearFields();
+	}
 
 	e.preventDefault();
 });
